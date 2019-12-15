3212f966bde20b6e118e2e24ac5bb8230526feb4
paul028 <paulvincentnonat@gmail.com>
2019-12-15 18:28:57 +0800

    state probability

--- markovdisk.py
+++ markovdisk.py
@@ -6,6 +6,14 @@ Created on Sat Dec  7 20:15:07 2019
 """
 import numpy as np
 
+#find P20, P21, P22 1st using b=Ax and steady state equation to set up b=AX where B represent the state transition probabilities of Write j=2
+a = np.array([[0,-0.1,0.04], [0.04,0.05,0],[0.01,0,0.05]])
+b = np.array([0.0199,-0.015,0.02])
+
+
+a = np.array([[0.04,0.05,0],[0.01,0,0.05],[0,-0.1,0.04]]
+
+b = np.array([-0.015,0.02,0199])
 mat = ([0.95,0.04,0.01],[0.5,0.9,0.5],[0.5,0.5,0.9])
 mat1=([0.5, 0.25, 0.25], [0.6666666666666666, 0, 0.3333333333333333], [0.6, 0.4, 0])
 
